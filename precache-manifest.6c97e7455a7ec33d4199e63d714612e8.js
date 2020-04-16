2c9df4d6e55cca32c47c35eae023224a75f868dd
Roy Amar <royammar030783@gmail.com>
2020-04-16 15:30:44 +0300

    Updates

---
+++ precache-manifest.6c97e7455a7ec33d4199e63d714612e8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad247662c4373a97515b16494da961d",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "4d2f437e8e05e2b87de6",
+    "url": "/movies/static/css/2.ddb5edb4.chunk.css"
+  },
+  {
+    "revision": "e6b7a54fa00aaabd4ecd",
+    "url": "/movies/static/css/main.ae607095.chunk.css"
+  },
+  {
+    "revision": "4d2f437e8e05e2b87de6",
+    "url": "/movies/static/js/2.569af0e1.chunk.js"
+  },
+  {
+    "revision": "413b595604c5d3d1d7baf18f18130db2",
+    "url": "/movies/static/js/2.569af0e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6b7a54fa00aaabd4ecd",
+    "url": "/movies/static/js/main.3ccd58ed.chunk.js"
+  },
+  {
+    "revision": "9a2b7e300d501944a1ac",
+    "url": "/movies/static/js/runtime-main.e2fdec1d.js"
+  },
+  {
+    "revision": "895f5b025a6cc4924b263f6beb06c777",
+    "url": "/movies/static/media/FiraSans-Regular.895f5b02.ttf"
+  },
+  {
+    "revision": "670e50b100f9426e8a78ca14b549ccfe",
+    "url": "/movies/static/media/chairs.670e50b1.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/movies/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/movies/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/movies/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/movies/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/%E2%80%8Bhttps://royammar.github.io/movies%E2%80%8B/precache-manifest.f3f2170c158d5050916fcad4094330df.js"
+  "/movies/precache-manifest.6c97e7455a7ec33d4199e63d714612e8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/%E2%80%8Bhttps://royammar.github.io/movies%E2%80%8B/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movies/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
