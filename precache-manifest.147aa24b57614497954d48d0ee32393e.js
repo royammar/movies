9d61bad779d59340194718c983d16cea9c8361d5
Roy Amar <royammar030783@gmail.com>
2020-04-16 15:17:19 +0300

    Updates

---
+++ precache-manifest.147aa24b57614497954d48d0ee32393e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cdcf701ef0104c687e1076c261fc01b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ddcbf170c3dcd3bd543",
+    "url": "/static/css/2.96797dfb.chunk.css"
+  },
+  {
+    "revision": "b73c8df8439b611a1c79",
+    "url": "/static/css/main.f09b1231.chunk.css"
+  },
+  {
+    "revision": "6ddcbf170c3dcd3bd543",
+    "url": "/static/js/2.569af0e1.chunk.js"
+  },
+  {
+    "revision": "413b595604c5d3d1d7baf18f18130db2",
+    "url": "/static/js/2.569af0e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b73c8df8439b611a1c79",
+    "url": "/static/js/main.760345e9.chunk.js"
+  },
+  {
+    "revision": "630c0ea7bac21dcf7625",
+    "url": "/static/js/runtime-main.482b80f8.js"
+  },
+  {
+    "revision": "895f5b025a6cc4924b263f6beb06c777",
+    "url": "/static/media/FiraSans-Regular.895f5b02.ttf"
+  },
+  {
+    "revision": "670e50b100f9426e8a78ca14b549ccfe",
+    "url": "/static/media/chairs.670e50b1.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
